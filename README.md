1c236f311aab544ff1b4d85b3c3182897785ccd3
HIN CHEU <106651561+HINCHEU@users.noreply.github.com>
2024-06-07 17:05:26 +0700

    Add verify email

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-### git
+### About
+**
+    Online Exam System is a system that allow teacher to create exam for doing with student.
+    This is laravel 10 intergrate with laravel breez 
+***
 
+
+
+### git clone
+**
 1. Clone From Github
     url: git clone
 
@@ -13,7 +21,9 @@
 3. Connect Project to database (.env)
 
 4. Migrate Database
+    (create mysql database name : online_exam_db )
     cmd: php artisan migrate:fresh --seed
 
 5. Have fun
     cmd: php artisan serve
+**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Carbon\Factory;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +23,42 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        // Role::factory()->create([
+        //     'name' => 'Admin'
+        // ]);
+        // Role::factory()->create([
+        //     'name' => 'Teacher'
+        // ]);
+        // Role::factory()->create([
+        //     'name' => 'Student'
+        // ]);
+
+        //Teacher User
+        User::factory()->create([
+            'name' => 'Teacher',
+            'email' => 'teacher@exam.com',
+            'role_id' => '2',
+            'email_verified_at' => '2024-05-29 06:11:1',
+            'password' => Hash::make('password')
+
+        ]);
+        //admin
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@exam.com',
+            'role_id' => '1',
+            'email_verified_at' => '2024-05-29 06:11:1',
+            'password' => Hash::make('password'),
+        ]);
+        //Student
+        User::factory()->create([
+            'name' => 'student',
+            'email' => 'student@exam.com',
+            'role_id' => '3',
+            'email_verified_at' => '2024-05-29 06:11:1',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
